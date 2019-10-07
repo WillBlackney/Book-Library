3be671acbd874613e54861f109123ee0148994e2
WillBlackney <44806865+WillBlackney@users.noreply.github.com>
2019-10-07 23:11:29 +0200

    Push 1

---
+++ Book Library/Book Library/Book.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book_Library
+{
+    public class Book
+    {
+        // Variables
+        #region
+        private string title;
+        private string author;
+        private string isbnCode;
+        private int numberOfPages;
+        #endregion
+        
+        // Properties
+        #region
+        public string Title
+        {
+            get { return title; }
+
+            set
+            {
+                if(value.Length < 2)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    { title = value; }
+                }
+            }
+                
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string IsbnCode
+        {
+            get { return isbnCode; }
+            set
+            {
+                if(value.Length != 13)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    isbnCode = value;
+                }
+            }
+                
+        }
+        public int NumberOfPages
+        {
+            get { return numberOfPages; }
+            set
+            {
+                if (value < 10 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    numberOfPages = value;
+                }
+                
+            }
+        }
+        #endregion
+
+        // Contructor
+        #region
+        public Book(string _title, string _author, string _isbnCode, int _numberOfPages)
+        {
+            Title = _title;
+            Author = _author;
+            IsbnCode = _isbnCode;
+            NumberOfPages = _numberOfPages;
+        }
+        #endregion
+
+    }
+}
